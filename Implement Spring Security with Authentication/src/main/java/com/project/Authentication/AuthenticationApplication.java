5ee0babf7d4880a18032e677aaf68b96082363e7
thadakaajay <97282042+thadakaajay@users.noreply.github.com>
2022-02-10 22:38:21 +0530

    Add files via upload

---
+++ Implement Spring Security with Authentication/src/main/java/com/project/Authentication/AuthenticationApplication.java
@@ -0,0 +1,25 @@
+package com.example.Authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.project.Authentication.controllers.AuthenticationController;
+import com.example.Authentication.entities.User;
+import com.project.Authentication.exceptions.UserNotFoundException;
+import com.example.Authentication.services.AuthenticationService;
+
+@SpringBootApplication
+@Import({
+      AuthenticationController.class,
+      UserNotFoundException.class,
+      AuthenticationService.class,
+      User.class
+})
+public class AuthenticationApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(AuthenticationApplication.class, args);
+   }
+
+}
